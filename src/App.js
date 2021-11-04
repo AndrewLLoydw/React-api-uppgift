532cabc5887442e0b495f649335779cd07a6c051
Andre Lillhannus <fasthuntlol@gmail.com>
2021-11-04 14:09:49 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import Customer from './components/Customer';
 
 function App() {
-  const [customers, setCustomers] = useState([])
+  const [listCustomers, setCustomerList] = useState([])
   useEffect(() => {
 
     async function fetchData() {
-      const res = await fetch("https://ecexam-webapi.azurewebsites.net/api/customers")
-      setCustomers(await res.json())
+      const response = await fetch("https://ecexam-webapi.azurewebsites.net/api/customers")
+      setCustomerList(await response.json())
     }
     fetchData()
   }, [])
 
   return (
-    <div className="mx-5 mt-4 row row-cols-3 d-flex">
+    <div className="mx-5 mt-4 row row-cols-2 d-flex">
       {
-        customers.map(customer => (      
+        listCustomers.map(customer => (      
         <div key={(customer.id)} className="list-group mb-3">
           <a href="#" className="shadow list-group-item list-group-item-action" aria-current="true">
             <Customer item={customer}/>
